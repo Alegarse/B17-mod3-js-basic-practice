87178c7cc78c81515aafa8f61de077e82fc84dfd
Alejandro García Serrano <agscm2@gmail.com>
2025-03-13 16:40:58 +0100

    Reestructirung & Upload Exercise 10

--- src/exercises/exercise-10.js
+++ src/exercises/exercise-10.js
@@ -4,8 +4,15 @@ Escribe una o varias funciones para que, dado un string, devuelva un array que c
 que ocupa cada carácter en el abecedario. Para que sea más sencillo, elimina los espacios y tildes 
 (recuerda que puedes utilizar funciones auxiliares de ejercicios anteriores si las declaraste en el archivo utils.js).
 */
+
+import { phraseLowerWithOutSpacesAndAcents, charPosition } from "../utils";
+
 export function exercise10(string) {
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
-
   // Escribe tu solución aquí
+  let arrayString = phraseLowerWithOutSpacesAndAcents(string).split('');
+  let arrayAlphabet = alphabet.split('');
+  let positionArray = [];
+  positionArray = charPosition(arrayString, arrayAlphabet, positionArray);
+  return positionArray;
 }
--- src/utils.js
+++ src/utils.js
@@ -25,6 +25,43 @@ export function stringLowerWithOutSpacesAndSigns(string) {
   return fixedString.join('');
 }
 
+
+/**
+ * 
+ * @param {*} string 
+ * @returns String lowered without spaces & accents
+ */
+export function phraseLowerWithOutSpacesAndAcents(string) {
+  /**
+   * ================================================================================================================
+   * PATRON REGEX
+   * ================================================================================================================
+   * Es un patron así llamado para búsquedas
+   * Siempre empieza por /
+   * Lo que queramos buscar va entre []
+   * Iniciamos patron de búsqueda con \ y después lo que queramos buscar, en este caso u para buscar carácter unicode
+   * Caracteres Unicode: https://symbl.cc/es/unicode-table/#combining-diacritical-marks
+   * Sintaxis expresiones: https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Regular_expressions/Cheatsheet
+   * Por defecto solo busca primera coincidencia, para buscar todas se pone despues de los corchetes /g
+   * ================================================================================================================
+   * ================================================================================================================
+   */
+  let regex = /[\u0301]/g
+  /**
+   * Explicación del patrón entre corchetes []:
+   * u de caracter unicode
+   * 0301 es el caracter unicode del acento agudo
+   * Para buscar un rango ponemos - en medio
+   * Ej: \u0301-\u0304
+   */
+  return string.toLowerCase().replaceAll(' ','').normalize('NFD').replace(regex,'');
+  /**
+   * Explicación de normalize
+   * Si ponemos que sea de tipo NFD, subdivide los caracteres entre la letra y su agregado, en este caso el acento
+   * Están ambos, aunque el console.log no los muestre
+   */
+}
+
 /**
  * 
  * @param {*} string 
@@ -37,6 +74,22 @@ export function deleteCentralChar(string,length) {
       return string.substring(0,centralIndex) + string.substring(centralIndex + 1);
   }
   return string;
+}
+
+/**
+ * 
+ * @param {*} arrayString 
+ * @param {*} arrayAlphabet 
+ * @param {*} positionArray 
+ * @returns Array with alphabet positions of parameter arrayString
+ */
+export function charPosition(arrayString, arrayAlphabet, positionArray) {
+  arrayString.forEach(char => {
+      arrayAlphabet.forEach(charAlpha => {
+          if (char === charAlpha) positionArray.push(arrayAlphabet.indexOf(charAlpha)+1);
+      });
+  });
+  return positionArray;
 }
 
 // NUMBERS FUNCTIONS
