48255a4ef922ff02abd20bf609a58a990996233f
Alejandro García Serrano <agscm2@gmail.com>
2025-03-15 10:42:18 +0100

    Upload exercise 11

--- src/exercises/exercise-11.js
+++ src/exercises/exercise-11.js
@@ -4,6 +4,19 @@ Escribe una función que, pasándole como parámetro el array de películas 'mov
 exportado en el archivo "data.js", devuelva un array con las peliculas ordenadas 
 por valoracion (propiedad "rating") de mayor a menor  
 */
+
+function sortMoviesSteps(movies) {
+  // 1. Paso función a sort con dos elementos
+  // 2. Comparo restando los valores rating de esos dos elementos
+  // 3. Resultados: 
+  //    - positivo, movie2 irá delante de movie1
+  //    - negativo, movie1 irá delante de movie2
+  //    - cero, ambos permanecen en la misma posición
+  return movies.sort((movie1, movie2) => movie2.rating - movie1.rating)
+}
+
 export function exercise11(movies) {
   // Escribe tu solución aquí
+  let moviesSorted = sortMoviesSteps(movies) 
+  return moviesSorted
 }
