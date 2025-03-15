c732ca654fe344323e09bedffdc501c7b673a505
Alejandro García Serrano <agscm2@gmail.com>
2025-03-15 20:30:21 +0100

    Upload exercise 14

--- src/exercises/exercise-14.js
+++ src/exercises/exercise-14.js
@@ -4,6 +4,15 @@ Escribe una función que pasándole "movies" y "categories" del archivo data.js
 como parámetros filtra las películas que pertenezcan a la categoria "Drama" 
 e incluye solo el título y la descripción.
 */
+
 export function exercise14(movies, categories) {
   // Escribe tu solución aquí
+  let arrayMoviesFiltered = []
+  movies.forEach(movie => {
+    if (movie.category === categories.drama) {
+      const { title, description } = movie
+      arrayMoviesFiltered.push({ title, description })
+    }
+  })
+  return arrayMoviesFiltered
 }
