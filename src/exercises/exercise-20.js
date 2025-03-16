a3f1abe4029042f80cb6b977549b735739a06404
Alejandro García Serrano <agscm2@gmail.com>
2025-03-16 15:10:35 +0100

    Upload exercise 20 part 1

--- src/exercises/exercise-20.js
+++ src/exercises/exercise-20.js
@@ -14,11 +14,34 @@ Ejemplo: Fibonacci(20) = 6765
 
 Nota: evita llamar a la función con números altos en la versión recursiva.
 */
+/**
+ * 
+ * fórmula
+ * fib(1) = 1;
+ * fib(2) = 1;
+ * firb(n) = fin(n-2) + fib(n-1)
+ * serie 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 ... 
+ */
 
 export function exercise20(n) {
   // Escribe tu solución aquí
+  if (n === 0) return 0;
+  if (n === 1 || n === 2) return 1;
+  return (exercise20(n - 2) + exercise20(n - 1));
 }
 
 export function exercise20Iterative(n) {
   // Escribe tu solución aquí
+  if (n === 0) return 0;
+  if (n === 1 || n === 2) return 1;
+  let ind1 = 1
+  let ind2 = 1
+  let acc = 0
+  for(let i = 3; i <= n; i++) {
+    acc += (ind1 + ind2)
+    let aux = ind1 + ind2
+    ind1 = ind2
+    ind2 = aux
+  }
+  return acc
 }
